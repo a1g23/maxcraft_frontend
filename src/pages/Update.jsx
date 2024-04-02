45c62291989e7489997aa20a911dfa4ddee1d1a6
Alex Greenberg <acgreenberg2@gmail.com>
2024-04-02 19:28:34 -0400

    completed update page with Form

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,6 +1,24 @@
+import { useLoaderData, Form } from "react-router-dom"
+
 const Update = (props) => {
+    const product = useLoaderData()
+    const id = product?.url.split("/")[4]
+
     return(
-        <h1>Update Page</h1>
+        <div>
+            <h1>Update {product.type}..</h1>
+            
+            <Form action={`/update/${id}/`} method="POST">
+                <input type="text" id="type" name="type" defaultValue={product.type}/>
+                <input type="text" id="image" name="image" defaultValue={product.image}/>
+                <input type="text" id="size" name="size" defaultValue={product.size}/>
+                <input type="number" id="price" name="price" defaultValue={product.price}/>
+                <button>Update</button>
+            </Form>
+            
+        </div>
+        
+
     )
 }
 
